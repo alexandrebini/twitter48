6d33b9227387f97b9fe5de71b40358693605da56
Alexandre Bini <alexandrebini@gmail.com>
2015-01-09 19:47:11 -0200

    codeclimate/travis

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Twitter48
+
+[![Code Climate](https://codeclimate.com/github/alexandrebini/twitter48/badges/gpa.svg)](https://codeclimate.com/github/alexandrebini/twitter48)
+
+[![Build Status](https://travis-ci.org/alexandrebini/twitter48.svg)](https://travis-ci.org/alexandrebini/twitter48)
--- spec/models/timeline_spec.rb
+++ spec/models/timeline_spec.rb
@@ -18,19 +18,3 @@ describe Timeline do
     end
   end
 end
-
-
-  # def find(username)
-  #   twitter_timeline = TwitterClient.user_timeline(username)
-  #   self.from_client twitter_timeline
-  # rescue Twitter::Error
-  #   nil
-  # end
-
-  # def from_client(twitter_timeline)
-  #   Timeline.new.tap do |timeline|
-  #     twitter_timeline.each do |twitter_tweet|
-  #       timeline << Tweet.from_client(twitter_tweet)
-  #     end
-  #   end
-  # end
